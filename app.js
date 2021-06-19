669b41be651e9362d099f657c2869fab0d68de33
Max <sveramaxim@gmail.com>
2021-06-18 22:29:17 -0500

    DB connection - mongoose

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const app = express();
 
 const tourRouter = require('./routes/toursRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // Middleware
-
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'development'){
 app.use(morgan('dev'));
+}
+
 app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
--- server.js
+++ server.js
@@ -1,8 +1,26 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successfully established');
+  });
 
-app.listen(3000, () => {
-  console.log('Port is listening at 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Port is listening at 3000`);
 });
